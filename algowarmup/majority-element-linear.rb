1ce2c4370c5b8f40c8325da4467e98bdf548bdef
carnag3kid7 <carnag3kid7@gmail.com>
2017-11-19 18:41:58 +0200

    refactor majority element recursive implementation

--- algowarmup/majority-element-linear.rb
+++ algowarmup/majority-element-linear.rb
@@ -1,21 +1,18 @@
 def get_majority_element(array)
-  return nil if array.size == 0
+  return nil if array.empty?
   return array[0] if array.size == 1
 
   half = array.size / 2
   left = get_majority_element(array[0, half])
   right = get_majority_element(array[half, array.size])
 
-  if left == right
-    return left
-  end
-  if array.count(left) > half
-    return left
-  end
-  if array.count(right) > half
-    return right
-  end
-  return nil
+  left if left == right
+
+  return left if array.count(left) > half
+
+  return right if array.count(right) > half
+
+  nil
 end
 
 puts get_majority_element([1, 1, 1, 3, 3, 3, 1])
